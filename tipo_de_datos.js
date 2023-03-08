8a6a8cba3f0fef26ce4059278895d9e73202b86a
Furfaroignacio <125997980+Furfaroignacio@users.noreply.github.com>
2023-03-08 14:37:22 -0300

    Add files via upload

---
+++ tipo_de_datos.js
@@ -0,0 +1,64 @@
+// tipo de dato numbre int
+
+let edad = 18;
+console.log(edad);
+
+// tipo de datos number float
+
+let precio = 1898.14;
+console.log(precio);
+
+// tipo de dato string comilla simple
+
+let nombre = 'nacho';
+console.log(nombre);
+
+// tipo de dato string con comilla doble
+
+let apellido = "furfaro";
+console.log(apellido);
+
+//tipo de dato booleano true
+
+let nacio_en_argentina = true;
+console.log(nacio_en_argentina);
+
+// tipo de dato booleano false
+
+let nacio_en_eeuu = false;
+console.log(nacio_en_eeuu);
+
+// tipo de dato NaN
+
+let not_a_number = NaN;
+console.log(not_a_number);
+
+// tipo de dato null
+
+let nulo = null;
+console.log(nulo);
+
+// tipo de dato undefined
+
+let sin_valor;
+console.log(sin_valor)
+
+// tipo de dato objeto
+
+let mi_computadora = {
+    Procesador : "Ryzen 5 3400g",
+    Ram : 16,
+    corre_juegos : true
+
+};
+console.log(mi_computadora);
+
+// tipo de dato array
+let juegos_favoritos = [
+    "Red dead redemention", 'the witcher 3', ' minecraft'
+];
+console.log(juegos_favoritos);
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,3 @@
+const apellido = ' furfaro';
+
+console.log (apellido);
